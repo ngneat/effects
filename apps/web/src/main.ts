0695e63e2009705407d5da9c0bf88302592dba91
Boetzkes, Yannick <code@yannickboetzkes.com>
2021-09-09 08:26:34 +0200

    feat(*): add actions dispatcher

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,8 +1,11 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { AppModule }   from './app/app.module';
 import { environment } from './environments/environment';
+import { devTools }    from '@ngneat/effects';
+
+devTools()
 
 if (environment.production) {
   enableProdMode();
