8f49032a6c380f47e60daf375f59767fb51618ee
Boetzkes, Yannick <code@yannickboetzkes.com>
2021-09-25 09:00:57 +0200

    fix(*): angular wrapper

--- apps/ng-web/src/app/lazy/+state/lazy.effects.ts
+++ apps/ng-web/src/app/lazy/+state/lazy.effects.ts
@@ -1,11 +1,17 @@
-import { Injectable }    from '@angular/core';
-import { createEffect }  from '@ngneat/effects';
+import { ChangeDetectorRef, Injectable } from '@angular/core';
+import { createEffect }                  from '@ngneat/effects';
 import { ofType }        from 'ts-action-operators';
 import { lazyActionTwo } from './lazy.actions';
 import { tap }           from 'rxjs';
 
 @Injectable()
 export class LazyEffects {
+
+  constructor(
+    private cd: ChangeDetectorRef
+  ) {
+    console.log("construct")
+  }
 
   lazyEffectOne$ = createEffect(actions => actions.pipe(
     ofType(lazyActionTwo),
--- libs/effects-ng/src/lib/effects-ng.module.ts
+++ libs/effects-ng/src/lib/effects-ng.module.ts
@@ -1,25 +1,112 @@
-import { Injector, ModuleWithProviders, NgModule, Type }                                                     from '@angular/core';
-import { _FEATURE_EFFECTS, _ROOT_EFFECTS, EFFECTS_MANAGER, FEATURE_EFFECT_INSTANCES, ROOT_EFFECT_INSTANCES } from './tokens';
-import { EffectsRootModule }                                                                                 from './effect-root.module';
-import { EffectsFeatureModule }                                                                              from './effect-feature.module';
-import {
-  EffectsConfig,
-  EffectsManager
-}                                                                                                            from '../../../effects/src/lib/effects-manager';
-import { Actions }                                                                                           from './actions';
-import { actions }                                                                                           from '@ngneat/effects';
+import { Inject, InjectionToken, Injector, ModuleWithProviders, NgModule, Type } from '@angular/core';
+import { EFFECTS_MANAGER }                                                       from './tokens';
+import { EffectsConfig, EffectsManager }                                         from '../../../effects/src/lib/effects-manager';
+import { isEffect }                                                              from '../../../effects/src/lib/utils';
+import { Actions }                                                               from './actions';
+import { actions }                                                               from '@ngneat/effects';
 
 const registeredEffects = new WeakSet();
 
+// @NgModule({})
+// export class EffectsNgModule {
+//
+//   static forRoot(
+//     rootEffects: Type<any>[] = [],
+//     config?: EffectsConfig
+//   ): ModuleWithProviders<EffectsRootModule> {
+//     return {
+//       ngModule: EffectsRootModule,
+//       providers: [
+//         {
+//           provide: EFFECTS_MANAGER,
+//           useFactory: () => new EffectsManager(config)
+//         },
+//         {
+//           provide: Actions,
+//           useValue: actions
+//         },
+//         rootEffects,
+//         {
+//           provide: _ROOT_EFFECTS,
+//           useValue: [rootEffects]
+//         },
+//         {
+//           provide: ROOT_EFFECT_INSTANCES,
+//           useFactory: createEffectInstances,
+//           deps: [Injector, _ROOT_EFFECTS]
+//         }
+//       ]
+//     };
+//   }
+//
+//   static forFeature(featureEffects: Type<any>[] = []): ModuleWithProviders<EffectsFeatureModule> {
+//     return {
+//       ngModule: EffectsFeatureModule,
+//       providers: [
+//         featureEffects,
+//         {
+//           provide: _FEATURE_EFFECTS,
+//           useValue: featureEffects,
+//           multi: true
+//         },
+//         {
+//           provide: FEATURE_EFFECT_INSTANCES,
+//           multi: true,
+//           useFactory: createEffectInstances,
+//           deps: [Injector, _FEATURE_EFFECTS]
+//         }
+//       ]
+//     };
+//   }
+// }
+
+export function createEffectInstances(
+  effectsManager: EffectsManager,
+  injector: Injector,
+  effectGroups: Type<any>[]
+): InstanceType<any>[] {
+  const mergedEffects: Type<any>[] = effectGroups;
+
+  const effectInstances = mergedEffects.reduce((acc, effect) => {
+    if (!registeredEffects.has(effect)) {
+      registeredEffects.add(effect);
+      acc.push(injector.get(effect));
+    }
+    return acc;
+  }, [] as InstanceType<any>);
+
+  console.log(effectInstances);
+
+  effectGroups.forEach(effectInstance => {
+    Object.keys(effectInstance).forEach(propertyName => {
+      const effect = effectInstance[propertyName];
+      if (isEffect(effect)) {
+        console.log('register', effect);
+        effectsManager.registerEffects([effect]);
+      }
+    });
+  });
+
+  return effectInstances;
+}
+
+export const EFFECTS_PROVIDERS = new InjectionToken('EFFECTS_PROVIDERS');
+
 @NgModule({})
 export class EffectsNgModule {
+  constructor(
+    @Inject(EFFECTS_PROVIDERS) providers: Type<any>[]
+  ) {
+
+  }
 
   static forRoot(
-    rootEffects: Type<any>[] = [],
+    providers: Type<any>[],
     config?: EffectsConfig
-  ): ModuleWithProviders<EffectsRootModule> {
+  ): ModuleWithProviders<EffectsNgModule> {
+
     return {
-      ngModule: EffectsRootModule,
+      ngModule: EffectsNgModule,
       providers: [
         {
           provide: EFFECTS_MANAGER,
@@ -29,57 +116,22 @@ export class EffectsNgModule {
           provide: Actions,
           useValue: actions
         },
-        rootEffects,
+        ...providers,
         {
-          provide: _ROOT_EFFECTS,
-          useValue: [rootEffects]
-        },
-        {
-          provide: ROOT_EFFECT_INSTANCES,
-          useFactory: createEffectInstances,
-          deps: [Injector, _ROOT_EFFECTS]
-        }
-      ]
-    };
-  }
-
-  static forFeature(featureEffects: Type<any>[] = []): ModuleWithProviders<EffectsFeatureModule> {
-    return {
-      ngModule: EffectsFeatureModule,
-      providers: [
-        featureEffects,
-        {
-          provide: _FEATURE_EFFECTS,
-          useValue: featureEffects,
-          multi: true
-        },
-        {
-          provide: FEATURE_EFFECT_INSTANCES,
+          provide: EFFECTS_PROVIDERS,
           multi: true,
-          useFactory: createEffectInstances,
-          deps: [Injector, _FEATURE_EFFECTS]
+          useFactory: (effectsManager, injector) =>
+            createEffectInstances(effectsManager, injector, providers),
+          deps: [EFFECTS_MANAGER, Injector]
         }
       ]
     };
   }
-}
 
-export function createEffectInstances(injector: Injector, effectGroups: Type<any>[][]): InstanceType<any>[] {
-  const mergedEffects: Type<any>[] = [];
-
-  for (const effectGroup of effectGroups) {
-    mergedEffects.push(...effectGroup);
+  static forFeature(
+    providers: (Type<any>)[]
+  ): ModuleWithProviders<EffectsNgModule> {
+    return EffectsNgModule.forRoot(providers);
   }
 
-  // todo we shouldn't use a map to avoid registering the effects twice;
-  // fix the underlying issue for feature is called twice
-  const effectInstances = mergedEffects.reduce((acc, effect) => {
-    if (!registeredEffects.has(effect)) {
-      registeredEffects.add(effect);
-      acc.push(injector.get(effect));
-    }
-    return acc;
-  }, [] as InstanceType<any>);
-
-  return effectInstances;
 }
