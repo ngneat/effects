b8c3bf5b5ad79f501d9d19b901cfe8425fef4cbd
Netanel Basal <NetanelBasal@users.noreply.github.com>
2021-09-30 22:36:00 +0300

    refactor: build lib

---
+++ libs/effects-ng/src/lib/tokens.ts
@@ -0,0 +1,4 @@
+import { InjectionToken } from "@angular/core";
+
+export const EFFECTS_PROVIDERS = new InjectionToken('EFFECTS_PROVIDERS');
+export const EFFECTS_MANAGER = new InjectionToken('@ngneat/effects Effects Manager');
