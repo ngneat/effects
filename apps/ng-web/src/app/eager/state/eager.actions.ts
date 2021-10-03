3eb29159c403c296ed788010755061bd17b177cd
Boetzkes, Yannick <code@yannickboetzkes.com>
2021-10-03 17:31:57 +0200

    chore(*): format

--- apps/ng-web/src/app/eager/state/eager.actions.ts
+++ apps/ng-web/src/app/eager/state/eager.actions.ts
@@ -1,7 +1,12 @@
 import { actionsFactory } from '@ngneat/effects';
 import { props }          from 'ts-action';
 
+interface EagerProps {
+  id: string;
+  name: string;
+}
+
 const eagerActions = actionsFactory('eager');
 
 export const eagerActionOne = eagerActions.create('One');
-export const eagerActionTwo = eagerActions.create('Two', props<{ eager: string }>());
+export const eagerActionTwo = eagerActions.create('Two', props<EagerProps>());
